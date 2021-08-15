993398d7e9d8a4caba201509fe1b45531a86ed99
benlau6 <benyplau@gmail.com>
2021-08-15 18:24:47 +0800

    disable CORS

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -11,18 +11,16 @@ from app.api.v1.api import router as v1_router
 def create_app(settings: config.Settings) -> FastAPI:
     app = FastAPI(root_path=settings.ROOT_STR)
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[
-            "http://localhost:8000",
-            "http://localhost:3000",
-            "http://localhost:9528",
-            "http://127.0.0.1:9528"
-        ],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    # add it back if you are not using reverse-proxy
+    # app.add_middleware(
+    #     CORSMiddleware,
+    #     allow_origins=[
+    #         "http://localhost:9528",
+    #     ],
+    #     allow_credentials=True,
+    #     allow_methods=["*"],
+    #     allow_headers=["*"],
+    # )
 
     # instead of include_router, so to separate differnt versions
     app.include_router(v1_router)
